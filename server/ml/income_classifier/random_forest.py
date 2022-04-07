f600062a3739ba5d743c56b1f40ac69e109217ec
Robert Songer <robert.songer@gmail.com>
2022-04-07 11:31:48 +0900

    Added ml app and code

---
+++ server/ml/income_classifier/random_forest.py
@@ -0,0 +1,99 @@
+import joblib
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from rest_framework.exceptions import APIException
+
+class RandomForestClassifier:
+    '''
+    A wrapper for the Random Forest model stored in the `training` folder.
+
+    Attributes:
+        values_fill_missing: a dictionary of mode values used to fill missing values in the input data
+        encoder: the encoder used to convert categorical data to numerical data in the input data
+        model: the trained random forest model
+    '''
+    
+    def __init__(self):
+        project_dir = Path(__file__).resolve().parent.parent.parent.parent
+        path_to_artifacts = Path(project_dir, "training")
+        self.values_fill_missing =  joblib.load(Path(path_to_artifacts, "train_mode.joblib"))
+        self.encoder = joblib.load(Path(path_to_artifacts, "encoder.joblib"))
+        self.model = joblib.load(Path(path_to_artifacts, "random_forest.joblib"))
+
+    def preprocessing(self, input_data):
+        '''
+        Preprocess the input data so it can be used with the model.
+        This method fills missing values and encodes categorical data using the same
+        objects that were used to preprocess the model's training data.
+
+        Args:
+            input_data: original data to be used as the basis for a prediction
+
+        Raises:
+            APIException: when multiple samples are given for prediction
+
+        Returns:
+            input_data: the original data after it has been processed to comply with the model
+        '''
+        # JSON to pandas DataFrame
+        input_data = pd.DataFrame(input_data, index=[0])
+        if input_data.shape[0] > 1:
+            raise APIException("Multiple samples provided to single sample prediction method")
+        # fill missing values
+        input_data.fillna(self.values_fill_missing)
+        # convert categoricals
+        categoricals = self.encoder.feature_names_in_
+        input_data_encoded = self.encoder.transform(input_data[categoricals])
+        input_data_encoded = pd.DataFrame(data=input_data_encoded, columns=categoricals)
+        for category in categoricals:
+            input_data[category] = input_data_encoded[category].values
+
+        return input_data
+
+    def predict(self, input_data):
+        '''
+        Use this Random Forest model to predict probabilities for income classes from the given data.
+
+        Args:
+            input_data: the preprocessed data to used in making the prediction
+
+        Returns:
+            predict_proba: a probability matrix that the income is greater than 50K.
+        '''
+        return self.model.predict_proba(input_data)
+
+    def postprocessing(self, input_data):
+        '''
+        Wrap the predicted probability and its respective label in a JSON response with OK status.
+
+        Args:
+            input_data: the probability matrix returned from the model's prediction method
+
+        Returns:
+            response: JSON object containing probability, label, and OK status
+        '''
+        label = "<=50K"
+        if input_data[1] > 0.5:
+            label = ">50K"
+        return {"probability": input_data[1], "label": label, "status": "OK"}
+
+    def compute_prediction(self, input_data):
+        '''
+        The endpoint function that combines preprocessing, prediction, and postprocessing
+        to generate a response.
+
+        Args:
+            input_data: the original sample data to be used in the prediction
+
+        Returns:
+            prediction: a JSON object containing prediction probability, label, and OK status
+        '''
+        try:
+            input_data = self.preprocessing(input_data)
+            prediction = self.predict(input_data)[0]  # only one sample
+            prediction = self.postprocessing(prediction)
+        except Exception as e:
+            return {"status": "Error", "message": str(e)}
+
+        return prediction
--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -8,9 +8,31 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from random import Random
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+
+# ML registry
+import inspect
+from ml.registry import MLRegistry
+from ml.income_classifier.random_forest import RandomForestClassifier
+
+try:
+    registry = MLRegistry()
+    rf = RandomForestClassifier()
+
+    registry.add_algorithm(endpoint_name="income_classifier",
+                           algorithm_object=rf,
+                           algorithm_name="Random Forest",
+                           algorithm_status="production",
+                           algorithm_version="0.0.1",
+                           owner="Rob",
+                           algorithm_description="Random Forest with simple pre- and post-processing.",
+                           algorithm_code=inspect.getsource(RandomForestClassifier))
+except Exception as e:
+    print(f"Exception while loading algorithms to the registry:\n>>\t{str(e)}")
